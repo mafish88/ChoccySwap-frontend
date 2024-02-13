71df5932a864a3942f8e51e23dfe906a7bc9dd50
FrancescoSnt <francesco.santanche@chromaway.com>
2024-02-13 18:15:50 +0000

    allowing build

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -1,8 +1,9 @@
 <div class="w-full h-full flx flex-col">
     <h3 class="text-[30pt]">
-        Not found
+        In development
     </h3>
     <p class="text-gray-50 text-center" style="white-space:pre-line">
-        The resource you're looking for does not exist.
+        The resource you're looking for is not yet accessible.
+        Please wait for the testing round to open.
     </p>
 </div>
