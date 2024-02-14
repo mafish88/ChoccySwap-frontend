d90d968cee60dc4196fe9c324e0a754773f4f714
FrancescoSnt <francesco.santanche@chromaway.com>
2024-02-14 20:34:59 +0000

    fixed text and overflow

--- src/lib/components/sections/features.svelte
+++ src/lib/components/sections/features.svelte
@@ -6,12 +6,42 @@
     import test4 from "$lib/images/test_4.png";
 
     const feats = [
-        {url: test1, text:"Zero gas transactions"},
-        {url: test2, text:"FT4 and ERC20 compatible"},
-        {url: test3, text:"Orderbook trading"},
-        {url: test4, text:"To be announced"},
-        {url: test4, text:"To be announced"},
-        {url: test4, text:"To be announced"},
+        {
+            url: test1,
+            size: "4rem",
+            text:"Zero gas",
+            explainer: "Using ChoccySwap only involves a liquidity fee."
+        },
+        {
+            url: test2,
+            size: "4rem",
+            text:"FT4 & ERC20 compatible",
+            explainer: "You can exchange between FT4 and ERC20 tokens."
+        },
+        {
+            url: test3,
+            size: "4rem",
+            text:"Orderbook trading",
+            explainer: "You can trade while you're logged off using the orderbook."
+        },
+        {
+            url: test4,
+            size: "4rem",
+            text:"To be announced",
+            explainer: "This feature is going to be announced soon. Stay tuned and follow us to know more!"
+        },
+        {
+            url: test4,
+            size: "4rem",
+            text:"To be announced",
+            explainer: "This feature is going to be announced soon. Stay tuned and follow us to know more!"
+        },
+        {
+            url: test4,
+            size: "4rem",
+            text:"To be announced",
+            explainer: "This feature is going to be announced soon. Stay tuned and follow us to know more!"
+        },
     ];
 </script>    
     
@@ -20,11 +50,16 @@
         <div class="grid mx-2"
             style="grid-template-columns:repeat({matches? "2" : "1"}, minmax(0, 1fr));">
             {#each feats as feat}
-                <span class="w-full h-full flx p-7 !justify-start border-gray-400 border">
-                    <img class="w-10 h-10 mr-2" src={feat.url} alt="" />
-                    <p class="italic text-gray-0 text-[15pt]">
-                        {feat.text}
-                    </p>
+                <span class="w-[350px] h-full flx p-7 !justify-start border-gray-400 border">
+                    <img style:height={feat.size} class="mr-2" src={feat.url} alt="" />
+                    <div class="flex flex-col align-center">
+                        <p class="italic text-gray-0 text-[15pt]">
+                            {feat.text}
+                        </p>
+                        <p class="italic text-gray-200 text-[10pt]">
+                            {feat.explainer}
+                        </p>
+                    </div>
                 </span>
             {/each}
         </div>
