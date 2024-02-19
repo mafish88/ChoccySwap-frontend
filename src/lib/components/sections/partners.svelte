ee8e0609fa6185927a3be46f139a95b8fc294ddd
FrancescoSnt <francesco.santanche@chromaway.com>
2024-02-19 18:35:59 +0000

    scroll pause on hover

--- src/lib/components/sections/partners.svelte
+++ src/lib/components/sections/partners.svelte
@@ -5,17 +5,23 @@
     );
 
     const imgs = Object.values(partners) as string[];
+
+    let stopped = false;
 </script>    
     
 <h3 class="text-[20pt] mb-8">Partners</h3>
-<div class="w-screen py-6 bg-gray-200">
+<div class="w-screen py-6 bg-gray-200" role="region"
+    on:mouseover={()=>{stopped=true}} on:focus={()=>{stopped=true}}
+    on:mouseleave={()=>{stopped=false}} on:blur={()=>{stopped=false}}>
     <div class="relative w-screen h-10">
-        <div class="absolute top-0 flex justify-around w-screen primary">
+        <div class="absolute top-0 flex justify-around w-screen primary"
+            style:animation-play-state={stopped? "paused" : "running"}>
             {#each imgs as img}
                 <img src={img} alt="partner-logo" class="h-10"/>
             {/each}
         </div>
-        <div class="absolute top-0 flex justify-around w-screen secondary">
+        <div class="absolute top-0 flex justify-around w-screen secondary"
+        style:animation-play-state={stopped? "paused" : "running"}>
             {#each imgs as img}
                 <img src={img} alt="partner-logo" class="h-10"/>
             {/each}
