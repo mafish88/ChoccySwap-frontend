d98911552a5583eefc30470debb60020242c5a4f
FrancescoSnt <francesco.santanche@chromaway.com>
2024-02-20 17:50:25 +0000

    fixed scroller

--- src/lib/components/sections/partners.svelte
+++ src/lib/components/sections/partners.svelte
@@ -14,36 +14,28 @@
     on:mouseover={()=>{stopped=true}} on:focus={()=>{stopped=true}}
     on:mouseleave={()=>{stopped=false}} on:blur={()=>{stopped=false}}>
     <div class="relative w-screen h-10">
-        <div class="absolute top-0 flex justify-around w-screen primary"
-            style:animation-play-state={stopped? "paused" : "running"}>
-            {#each imgs as img}
-                <img src={img} alt="partner-logo" class="h-10"/>
-            {/each}
-        </div>
-        <div class="absolute top-0 flex justify-around w-screen secondary"
-        style:animation-play-state={stopped? "paused" : "running"}>
-            {#each imgs as img}
-                <img src={img} alt="partner-logo" class="h-10"/>
-            {/each}
-        </div>
+        <div class="absolute min-w-[200vw] w-[max-content] top-0">
+            <div class="relative flex primary min-w-[200vw] justify-around"
+                style:animation-play-state={stopped? "paused" : "running"}>
+                {#each imgs as img}
+                    <img src={img} alt="partner-logo" class="h-10 inline mx-8"/>
+                {/each}
+                {#each imgs as img}
+                    <img src={img} alt="partner-logo" class="h-10 inline mx-8"/>
+                {/each}
+            </div>
+        </div> 
     </div>
 </div>
 
 <style>
     .primary {
+        left: 0;
         animation: scrollx 10s infinite linear;
     }
 
     @keyframes scrollx {
-        from { left: 0%; }
-        to { left: -100%; }
-    }
-    .secondary {
-        animation: scrollx2 10s infinite linear;
-    }
-
-    @keyframes scrollx2 {
-        from { left: 100%; }
-        to { left: 0%; }
+        from { left: 0; }
+        to { left: -50%; }
     }
 </style>
